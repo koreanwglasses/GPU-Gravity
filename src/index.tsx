b30748e5ac3dd08282dc34c1672170ca2f28dcf3
koreanwglasses <koreanwglasses@gmail.com>
2020-05-27 15:29:26 -0400

    added basic gravity slider

--- src/index.tsx
+++ src/index.tsx
@@ -10,48 +10,78 @@ import {
 import { renderToCanvas } from "./view/canvas-renderer";
 import { stepCollisions } from "./model/collisions";
 
-(async () => {
-  console.debug(`[Global] TF Backend: ${tf.getBackend()}`);
+function boxMuller() {
+  let u1 = Math.random();
+  let u2 = Math.random();
 
-  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  const ctx = canvas.getContext("2d");
+  let r = Math.sqrt(-2 * Math.log(u1));
+  let t = 2 * Math.PI * u2;
 
-  let bodies: PhysicsBody[] = [
-    { posX: 200, posY: 400, velX: 0, velY: 200, radius: 50 },
-    { posX: 600, posY: 400, velX: 0, velY: -200, radius: 50 },
-    { posX: 100, posY: 100, velX: 100, velY: 0, radius: 50 }
-  ];
+  let z0 = r * Math.cos(t);
+  let z1 = r * Math.sin(t);
 
-  let data = bodiesToTensor(bodies);
+  return [z0, z1] as [number, number];
+}
 
-  async function animate() {
-    // step the simulation
-    data = updateTensor(data, data =>
-      stepGravity(data, { dt: 1 / 60, gravConst: 1e7, dragCoeff: 0.1 })
-    );
-    data = updateTensor(data, data =>
-      stepBoundary(data, { maxX: canvas.width, maxY: canvas.height })
-    );
+function randomBody(maxX: number, maxY: number): PhysicsBody {
+  let posX = Math.random() * maxX;
+  let posY = Math.random() * maxY;
 
-    // Retrieve data to draw
-    bodies = await tensorToBodies(data, bodies);
+  let [velX, velY] = boxMuller();
+  velX *= 20;
+  velY *= 20;
 
-    // apply collisions
-    stepCollisions(bodies);
-    data.dispose();
-    data = bodiesToTensor(bodies);
+  let radius = Math.random() * 20 + 10;
 
-    // Draw
-    ctx.fillStyle = "rgba(255, 255, 255, 0.1)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "black";
-    renderToCanvas(ctx, bodies);
+  return { posX, posY, velX, velY, radius };
+}
 
-    requestAnimationFrame(animate);
+let gravConst = 1e6;
+(document.getElementById("grav-slider") as HTMLInputElement).addEventListener(
+  "input",
+  function() {
+    gravConst = Math.pow(10, +this.value);
   }
+);
 
-  animate();
+console.debug(`[Global] TF Backend: ${tf.getBackend()}`);
 
-  setInterval(() => console.log(tf.memory()), 5000);
-  // data.dispose()
-})();
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+let bodies: PhysicsBody[] = new Array(10)
+  .fill(null)
+  .map(() => randomBody(canvas.width, canvas.height));
+
+let data = bodiesToTensor(bodies);
+
+async function animate() {
+  // step the simulation
+  data = updateTensor(data, data =>
+    stepGravity(data, { dt: 1 / 60, gravConst, dragCoeff: 0.1 })
+  );
+  data = updateTensor(data, data =>
+    stepBoundary(data, { maxX: canvas.width, maxY: canvas.height })
+  );
+
+  // Retrieve data to draw
+  bodies = await tensorToBodies(data, bodies);
+
+  // apply collisions
+  stepCollisions(bodies);
+  data.dispose();
+  data = bodiesToTensor(bodies);
+
+  // Draw
+  ctx.fillStyle = "rgba(255, 255, 255, 0.1)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "black";
+  renderToCanvas(ctx, bodies);
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+setInterval(() => console.log(tf.memory()), 5000);
+// data.dispose()
